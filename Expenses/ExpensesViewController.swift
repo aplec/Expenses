84b64ee5f955ee6adaebc6f2a43a77b0aa5ed66d
Ante Plecas <anteplecas@mwc-019229.dhcp.missouri.edu>
2019-11-08 20:07:47 -0600

    Added expenses and core data

--- Expenses/ExpensesViewController.swift
+++ Expenses/ExpensesViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import CoreData
 
 class ExpensesViewController: UIViewController {
     
     @IBOutlet weak var expensesTableView: UITableView!
     
     let dateFormatter = DateFormatter()
+    
+    var expenses = [Expense]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,24 +24,57 @@ class ExpensesViewController: UIViewController {
         dateFormatter.dateStyle = .long
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else{
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest: NSFetchRequest<Expense> = Expense.fetchRequest()
+        
+        do {
+            expenses = try managedContext.fetch(fetchRequest)
+            
+            expensesTableView.reloadData()
+        } catch {
+            print("Fetch could not be performed")
+        }
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBAction func addNewExpense(_ sender: Any) {
-        
+        performSegue(withIdentifier: "showExpense", sender: self)
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let destination = segue.destination as? SingleExpenseViewController,
+            let selectedRow = self.expensesTableView.indexPathForSelectedRow?.row else {
+                return
+        }
+        
+        destination.existingExpense = expenses[selectedRow]
+    }
 }
 
 extension ExpensesViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return expenses.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = expensesTableView.dequeueReusableCell(withIdentifier: "expenseCell", for: indexPath)
+        let expense = expenses[indexPath.row]
+        
+        cell.textLabel?.text = expense.name
+        
+        if let date = expense.date {
+            cell.detailTextLabel?.text = dateFormatter.string(from: date)
+        }
         
         return cell
     }
